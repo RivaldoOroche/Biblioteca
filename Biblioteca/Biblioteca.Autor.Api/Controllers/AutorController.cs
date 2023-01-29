0edeeeb2729087df36b88747a0fdf0b17f82b654
Cesar Estrella <csluis97@gmail.com>
2023-01-28 22:54:39 -0500

    Prueba Autor, Agregando y Listando

---
+++ Biblioteca/Biblioteca.Autor.Api/Controllers/AutorController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MongoDB.Bson;
+using MongoDB.Driver;
+using dominio = Biblioteca.Autor.Dominio.Entidades;
+using static Biblioteca.Autor.Api.Routes.ApiRoutes;
+using Biblioteca.Autor.Aplicacion.Autor;
+
+namespace Biblioteca.Autor.Api.Controllers
+{
+    [ApiController]
+    public class AutorController : ControllerBase
+    {
+
+        private readonly IAutorService _service;
+
+        public AutorController(IAutorService service)
+        {
+            _service = service;
+        }
+
+        [HttpGet(RouteAutor.GetAll)]
+        public IEnumerable<dominio.Autor> ListarAutors()
+        {
+
+            var listaAutor = _service.ListarAutors();
+            return listaAutor;
+        }
+
+        [HttpGet(RouteAutor.GetById)]
+        public dominio.Autor BuscarAutor(int id)
+        {
+            var objAutor = _service.Autor(id);
+
+            return objAutor;
+        }
+
+        [HttpPost(RouteAutor.Create)]
+        public ActionResult<dominio.Autor> CrearAutor([FromBody] dominio.Autor Autor)
+        {
+            _service.RegistrarAutor(Autor);
+
+            return Ok();
+        }
+
+        [HttpDelete(RouteAutor.Delete)]
+        public ActionResult<dominio.Autor> EliminarAutor(int id)
+        {
+            _service.Eliminar(id);
+
+            return Ok(id);
+        }
+    }
+}
---
+++ Biblioteca/Biblioteca.Autor.Api/Routes/ApiRoutes.cs
@@ -0,0 +1,27 @@
+﻿namespace Biblioteca.Autor.Api.Routes
+{
+    public static class ApiRoutes
+    {
+        public const string Root = "api";
+        public const string Version = "v1";
+        public const string Base = Root + "/" + Version;
+
+        //asdasdas
+        public static class RouteAutor
+        {
+            //Para Leer
+            public const string GetAll = Base + "/autor/all";
+            public const string GetById = Base + "/autor/{id}";
+
+            //Write
+            public const string Create = Base + "/autor/create";
+            public const string Update = Base + "/autor/update";
+            public const string Delete = Base + "/autor/delete";
+
+
+
+        }
+
+
+    }
+}
